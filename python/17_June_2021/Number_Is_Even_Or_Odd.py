3d2d110ee97e9126f1e94b7c263c04108ae14401
savatabachhav <34607163+savatabachhav@users.noreply.github.com>
2021-06-20 11:22:07 +0530

    committing 17_June_2021 python practicals

---
+++ python/17_June_2021/Number_Is_Even_Or_Odd.py
@@ -0,0 +1,15 @@
+"""
+Name.: 		Bachhav Savata B.
+Class.:		MCA-I
+Roll No.:	03
+Subject:	Python Programming
+Program: 	Write a program to check if a number is even or odd
+"""
+
+
+number=int(input("Please Enter a Number : "));  
+x=int(number/2)*2;  
+if(x==number):  
+    print("This Number is Even")    
+else:  
+    print("This Number is Odd")  
